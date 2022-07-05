9c304ed5b460491133c5f95cd3a62de0c512d149
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-07-05 14:16:47 +0200

    end exercise

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="La mia prima app con Vue CLI!"/>
+    <ImageVue />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+import ImageVue from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
+    ImageVue
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  img{
+    width: 400px;
+    border: 2px solid red;
+    height: 300px;
+  }
+</style>
